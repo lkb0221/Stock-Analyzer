2ff67050f6d3a434e62e01b8b6d8bf3a775e5ff6
SZ <zheng@originlab.com>
2016-09-30 17:37:00 -0400

    09/30/2016

--- StockAnalyzer.c
+++ StockAnalyzer.c
@@ -146,11 +146,23 @@ Tree StockAnalyzer_GUI() {
 			GETN_NUM(Period, "Period", 14)						GETN_OPTION_NUM_FORMAT( ".0" )
 		GETN_END_BRANCH(EMV)
 		
+		GETN_BEGIN_BRANCH(Force, "Force Index")
+			GETN_NUM(Period, "Period", 13)						GETN_OPTION_NUM_FORMAT( ".0" )
+		GETN_END_BRANCH(Force)
+		
+		GETN_BEGIN_BRANCH(Mass, "Mass Index")
+			GETN_NUM(Period, "Period", 25)						GETN_OPTION_NUM_FORMAT( ".0" )
+		GETN_END_BRANCH(Mass)
+		
 		GETN_BEGIN_BRANCH(MACD, "Moving Average Convergence/Divergence Oscillator (MACD)")
 			GETN_NUM(WinEMA1, "EMA Window 1", 12)		GETN_OPTION_NUM_FORMAT( ".0" )
 			GETN_NUM(WinEMA2, "EMA Window 2", 26)		GETN_OPTION_NUM_FORMAT( ".0" )
 			GETN_NUM(SignalPeriod, "Signal Period", 9)			GETN_OPTION_NUM_FORMAT( ".0" )
 		GETN_END_BRANCH(MACD)
+		
+		GETN_BEGIN_BRANCH(MFI, "Money Flow Index (MFI)")
+			GETN_NUM(Period, "Period", 14)						GETN_OPTION_NUM_FORMAT( ".0" )
+		GETN_END_BRANCH(MFI)
 		
 		GETN_BEGIN_BRANCH(McClellan, "McClellan Oscillator")
 			GETN_NUM(Period1, "Period Window 1", 19)		GETN_CURRENT_SUBNODE.Enable = false;
@@ -414,6 +426,23 @@ void StockAnalyzer_MainProcess(int nUID) {
 	// EMV
 	vsIndex = StockAnalyzer_Offset(nIndexIndicator, 1);	
 	StockAnalyzer_EMV_Main(wksIndicator, wksData, vsIndex, trIndicators.EMV);
+	
+	// Force
+	vsIndex = StockAnalyzer_Offset(nIndexIndicator, 1);	
+	StockAnalyzer_Force_Main(wksIndicator, wksData, vsIndex, trIndicators.Force);
+	
+	// Mass
+	vsIndex = StockAnalyzer_Offset(nIndexIndicator, 1);	
+	StockAnalyzer_Mass_Main(wksIndicator, wksData, vsIndex, trIndicators.Mass);
+	
+	// MACD
+	vsIndex = StockAnalyzer_Offset(nIndexIndicator, 3);	
+	StockAnalyzer_MACD_Main(wksIndicator, wksData, vsIndex, trIndicators.MACD);
+	
+	// MFI
+	vsIndex = StockAnalyzer_Offset(nIndexIndicator, 1);	
+	StockAnalyzer_MFI_Main(wksIndicator, wksData, vsIndex, trIndicators.MFI);
+	
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1075,6 +1104,173 @@ static vector<double> StockAnalyzer_EMV(vector<double> vsHigh, vector<double> vs
 	return vsEMV;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Force Index
+
+static void StockAnalyzer_Force_Main(Worksheet wksTarget, Worksheet wksSource, vector<int> nIndex, Tree tr) {
+	Dataset dsClose(wksSource, 6);	// adj close
+	Dataset dsVolume(wksSource, 5);
+	
+	int nPeriod = tr.GetNode("Period").nVal;			
+	
+	Dataset dsForce(StockAnalyzer_SetColumn(wksTarget, nIndex[0], "Force Index", StockAnalyzer_Legend("FORCE", nPeriod)));
+	dsForce = StockAnalyzer_Force(dsClose, dsVolume, nPeriod);
+}
+
+static vector<double> StockAnalyzer_Force(vector<double> vsClose, vector<double> vsVol, int nPeriod = 14) {
+	// Force Index(1) = {Close (today)  -  Close (yesterday)} x Volume
+	// Force Index(N) = N-period EMA of Force Index(1)
+	
+	vector<double> vsForce;
+	
+	vector<double> vsPreviousClose;
+	vsClose.GetSubVector(vsPreviousClose, 0, vsClose.GetSize()-2);
+	vsPreviousClose.InsertAt(0, NANUM);
+	vsForce = (vsClose - vsPreviousClose) * vsVol;
+	vsForce = StockAnalyzer_EMA(vsForce, nPeriod);
+	
+	return vsForce;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Mass Index
+
+static void StockAnalyzer_Mass_Main(Worksheet wksTarget, Worksheet wksSource, vector<int> nIndex, Tree tr) {
+	Dataset dsHigh(wksSource, 2);		// 
+	Dataset dsLow(wksSource, 3);		// 
+	
+	int nPeriod = tr.GetNode("Period").nVal;	
+	
+	Dataset dsMass(StockAnalyzer_SetColumn(wksTarget, nIndex[0], "Mass Index", StockAnalyzer_Legend("MASS", nPeriod)));
+	dsMass = StockAnalyzer_Mass(dsHigh, dsLow, nPeriod);
+}
+
+static vector<double> StockAnalyzer_Mass(vector<double> vsHigh, vector<double> vsLow, int nPeriod = 25) {
+	vector<double> vsMass;
+	
+	vector<double> vsDiff, vsSingleEMA, vsDoubleEMA, vsRatioEMA;
+	vsDiff = vsHigh - vsLow;
+	vsSingleEMA = StockAnalyzer_EMA(vsDiff, 9);
+	vsDoubleEMA = StockAnalyzer_EMA(vsSingleEMA, 9);
+	vsRatioEMA = vsSingleEMA / vsDoubleEMA;
+	vsMass = StockAnalyzer_SMA(vsRatioEMA, nPeriod) * nPeriod;
+	
+	return vsMass;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//MACD (Moving Average Convergence/Divergence Oscillator)
+
+static void StockAnalyzer_MACD_Main(Worksheet wksTarget, Worksheet wksSource, vector<int> nIndex, Tree tr) {
+	Dataset dsClose(wksSource, 6);	// adj close
+	
+	int nPeriod1 = tr.GetNode("WinEMA1").nVal;
+	int nPeriod2 = tr.GetNode("WinEMA2").nVal;
+	int nPeriod3 = tr.GetNode("SignalPeriod").nVal;
+	
+	Dataset dsMACD(StockAnalyzer_SetColumn(wksTarget, nIndex[0], "Moving Average Convergence/Divergence Oscillator", StockAnalyzer_Legend("MACD", nPeriod1, nPeriod2,nPeriod3)));
+	Dataset dsSignal(StockAnalyzer_SetColumn(wksTarget, nIndex[1], "Moving Average Convergence/Divergence Oscillator", ""));
+	Dataset dsHisto(StockAnalyzer_SetColumn(wksTarget, nIndex[2], "Moving Average Convergence/Divergence Oscillator", ""));
+	
+	dsMACD = StockAnalyzer_MACD(dsClose, nPeriod1, nPeriod2);
+	dsSignal = StockAnalyzer_EMA(dsMACD, nPeriod3);
+	dsHisto = dsMACD - dsSignal;
+}
+
+static vector<double> StockAnalyzer_MACD(vector<double> vsClose, int nPeriod1 = 12, int nPeriod2 = 26) {
+	vector<double> vsMACD;
+	
+	vsMACD = StockAnalyzer_EMA(vsClose, nPeriod1) - StockAnalyzer_EMA(vsClose, nPeriod2);
+	
+	return vsMACD;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Money Flow Index (MFI)
+
+static void StockAnalyzer_MFI_Main(Worksheet wksTarget, Worksheet wksSource, vector<int> nIndex, Tree tr) {
+	Dataset dsHigh(wksSource, 2);		// 
+	Dataset dsLow(wksSource, 3);		// 
+	Dataset dsClose(wksSource, 4);	// real close
+	Dataset dsVolume(wksSource, 5);
+	
+	int nPeriod = tr.GetNode("Period").nVal;
+	
+	Dataset dsMFI(StockAnalyzer_SetColumn(wksTarget, nIndex[0], "Money Flow Index", StockAnalyzer_Legend("MFI", nPeriod)));
+	dsMFI = StockAnalyzer_MFI(dsHigh, dsLow, dsClose, dsVolume, nPeriod);
+}
+
+static vector<double> StockAnalyzer_MFI(vector<double> vsHigh, vector<double> vsLow, vector<double> vsClose, vector<double> vsVolume, int nPeriod) {
+	vector<double> vsMFI;
+	
+	vector<int> vsDir;
+	vector<double> vsTP, vsMF, vsMFr;
+	vsDir = StockAnalyzer_UpDown(vsClose);
+	vsTP = StockAnalyzer_TypicalPrice(vsHigh, vsLow, vsClose);
+	vsMF = StockAnalyzer_MoneyFlow(vsTP, vsVolume);
+	vsMFr = StockAnalyzer_MoneyFlowRatio(vsMF, vsDir, nPeriod);
+	vsMFI = StockAnalyzer_MoneyFlowIndex(vsMFr);
+	
+	return vsMFI;
+}
+
+static vector<int> StockAnalyzer_UpDown(vector<double> vsClose) {
+	int nSize = vsClose.GetSize();
+	vector<int> vsDir(nSize);
+	vsDir = NANUM;
+	
+	for (int ii = 1; ii < nSize; ii++) {
+		if (vsClose[ii] > vsClose[ii-1]) {
+			vsDir[ii] = 1;
+		}
+		else {
+			vsDir[ii] = -1;
+		}
+	};
+	
+	return vsDir;
+}
+
+static vector<double> StockAnalyzer_TypicalPrice(vector<double> vsHigh, vector<double> vsLow, vector<double> vsClose) {
+	return (vsHigh + vsLow + vsClose) / 3;
+}
+
+static vector<double> StockAnalyzer_MoneyFlow(vector<double> vsTypicalPrice, vector<double> vsVolume) {
+	return vsTypicalPrice * vsVolume;
+}
+
+static vector<double> StockAnalyzer_MoneyFlowRatio(vector<double> vsMF, vector<int> vsDir, int nPeriod) {
+	int nSize = vsMF.GetSize();
+	vector<double> vsMFp(nSize), vsMFn(nSize), vsMFr(nSize);
+	StockAnalyzer_SeperatePnN(vsMF, vsDir, vsMFp, vsMFn);
+	vsMFp = StockAnalyzer_SMA(vsMFp, nPeriod) * nPeriod;
+	vsMFn = StockAnalyzer_SMA(vsMFn, nPeriod) * nPeriod;
+	vsMFr = vsMFp / vsMFn;
+	
+	return vsMFr;
+}
+
+static vector<double> StockAnalyzer_MoneyFlowIndex(vector<double> vsMoneyFlowRatio) {
+	return 100 - 100 / (1 + vsMoneyFlowRatio);
+}
+
+static void StockAnalyzer_SeperatePnN(vector<double> vs, vector<int> vsDir, vector<double> &vsP, vector<double> &vsN) {
+	int nSize = vs.GetSize();
+	vsP = NANUM;
+	vsN = NANUM;
+	for (int ii = 0; ii < nSize; ii++) {
+		if (vsDir[ii] >= 0) {
+			vsP[ii] = vs[ii];
+		}
+		else {
+			vsN[ii] = vs[ii];
+		}
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 
@@ -1229,6 +1425,12 @@ static vector<string> SA_IndicatorList() {
 	sa.Add("Chaikin Oscillator");
 	sa.Add("PMO");
 	sa.Add("DPO");
+	sa.Add("Ease of Movement");
+	sa.Add("Force Index");
+	sa.Add("Mass Index");
+	sa.Add("MACD");
+	sa.Add("Money Flow Index");
+	sa.Add("");
 	sa.Add("");
 	sa.Add("Rate of Change");
 	
